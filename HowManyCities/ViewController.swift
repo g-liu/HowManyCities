35c336aeca9628cfa0853a810dd0781ec596410e
Geoffrey Liu <g-liu@noreply.users.github.com>
2022-03-29 11:32:04 -0700

    Map customization
    TODO: GET RID OF LABELS

--- HowManyCities/ViewController.swift
+++ HowManyCities/ViewController.swift
@@ -12,7 +12,14 @@ final class ViewController: UIViewController {
   
   private lazy var mapView: MKMapView = {
     let map = MKMapView().autolayoutEnabled
-    
+    map.mapType = .mutedStandard
+    map.isPitchEnabled = false
+    map.isRotateEnabled = false
+//    map.setCenter(.init(latitude: 0, longitude: 0), animated: true)
+    map.setRegion(.init(center: .init(latitude: 0, longitude: 0), span: .init(latitudeDelta: 180, longitudeDelta: 360)), animated: true)
+    map.setCameraZoomRange(.init(minCenterCoordinateDistance: 1000000), animated: true)
+    map.removeAnnotations(map.annotations)
+    map.pointOfInterestFilter = .excludingAll
     return map
   }()
 
