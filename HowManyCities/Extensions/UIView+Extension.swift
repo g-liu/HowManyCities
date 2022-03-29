bec13aee8678ed439466cd35a0fc7a0ecca09bbd
Geoffrey Liu <g-liu@noreply.users.github.com>
2022-03-29 11:11:41 -0700

    POC display the map

---
+++ HowManyCities/Extensions/UIView+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  UIView+Extension.swift
+//  HowManyCities
+//
+//  Created by Geoffrey Liu on 3/29/22.
+//
+
+import UIKit
+
+extension UIView {
+  var autolayoutEnabled: Self {
+    translatesAutoresizingMaskIntoConstraints = false
+    return self
+  }
+  
+  func pin(to otherView: UIView, margins: UIEdgeInsets = .zero) {
+    NSLayoutConstraint.activate([
+      topAnchor.constraint(equalTo: otherView.topAnchor, constant: margins.top),
+      bottomAnchor.constraint(equalTo: otherView.bottomAnchor, constant: -margins.bottom),
+      leadingAnchor.constraint(equalTo: otherView.leadingAnchor, constant: margins.left),
+      trailingAnchor.constraint(equalTo: otherView.trailingAnchor, constant: -margins.right),
+    ])
+  }
+  
+  func pin(to layoutGuide: UILayoutGuide, margins: UIEdgeInsets = .zero) {
+    NSLayoutConstraint.activate([
+      topAnchor.constraint(equalTo: layoutGuide.topAnchor, constant: margins.top),
+      bottomAnchor.constraint(equalTo: layoutGuide.bottomAnchor, constant: -margins.bottom),
+      leadingAnchor.constraint(equalTo: layoutGuide.leadingAnchor, constant: margins.left),
+      trailingAnchor.constraint(equalTo: layoutGuide.trailingAnchor, constant: -margins.right),
+    ])
+  }
+}
+
--- HowManyCities/ViewController.swift
+++ HowManyCities/ViewController.swift
@@ -6,14 +6,29 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+  
+  private lazy var mapView: MKMapView = {
+    let map = MKMapView().autolayoutEnabled
+    
+    return map
+  }()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
     
     view.backgroundColor = .systemBackground
+    
+    view.addSubview(mapView)
+    NSLayoutConstraint.activate([
+      mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+      mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+    ])
   }
 
 
