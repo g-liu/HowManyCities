2c92301a40fce9423bfa0a730cf1504e25e9fd1e
Geoffrey Liu <g-liu@noreply.users.github.com>
2022-03-29 11:41:00 -0700

    Textfield add

--- HowManyCities/ViewController.swift
+++ HowManyCities/ViewController.swift
@@ -15,12 +15,24 @@ final class ViewController: UIViewController {
     map.mapType = .mutedStandard
     map.isPitchEnabled = false
     map.isRotateEnabled = false
-//    map.setCenter(.init(latitude: 0, longitude: 0), animated: true)
     map.setRegion(.init(center: .init(latitude: 0, longitude: 0), span: .init(latitudeDelta: 180, longitudeDelta: 360)), animated: true)
     map.setCameraZoomRange(.init(minCenterCoordinateDistance: 1000000), animated: true)
     map.removeAnnotations(map.annotations)
     map.pointOfInterestFilter = .excludingAll
     return map
+  }()
+  
+  private lazy var cityInputTextField: UITextField = {
+    let textField = UITextField().autolayoutEnabled
+    textField.delegate = self
+    textField.layer.borderWidth = 1
+    textField.layer.borderColor = UIColor.systemFill.cgColor
+    textField.font = .systemFont(ofSize: 36)
+    textField.textAlignment = .center
+    
+    textField.autocorrectionType = .no
+  
+    return textField
   }()
 
   override func viewDidLoad() {
@@ -30,14 +42,33 @@ final class ViewController: UIViewController {
     view.backgroundColor = .systemBackground
     
     view.addSubview(mapView)
+    view.addSubview(cityInputTextField)
     NSLayoutConstraint.activate([
       mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-      mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+      mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -64),
       mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
       mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+      
+      cityInputTextField.topAnchor.constraint(equalTo: mapView.bottomAnchor, constant: 32),
+      cityInputTextField.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, constant: -32),
+      cityInputTextField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
     ])
+    
+    cityInputTextField.becomeFirstResponder()
   }
-
 
 }
 
+extension ViewController: UITextFieldDelegate {
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    guard textField == cityInputTextField,
+          let textInput = textField.text,
+          !textInput.isEmpty else { return false }
+    
+    textField.text = ""
+    
+    // TODO: Submit text as api
+    
+    return false
+  }
+}
