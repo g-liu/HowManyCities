5ad58976f8354a4d1d2a0eaf758d55444a522e54
Geoffrey Liu <g-liu@noreply.users.github.com>
2022-03-30 16:22:04 -0700

    Draw capital stars to scale

--- HowManyCities/Data/Cities.swift
+++ HowManyCities/Data/Cities.swift
@@ -36,18 +36,21 @@ struct City: Codable, Hashable {
     .init(latitude: latitude, longitude: longitude)
   }
   
+  // The circle size is proportional to the city's population, on a logarithmic scale
+  private var absoluteSize: CLLocationDistance {
+    200_000 * log10(0.000_019*(population+100_000))+13_000
+  }
+  
   private var circleSize: CLLocationDistance {
-    // The circle size is proportional to the city's population, on a logarithmic scale
-    let absoluteSize = 200_000 * log10(0.000_019*(population+100_000))+13_000
     // We must account for the distortion caused by the Mercator projection, otherwise
     // cities closer to the poles will appear to have larger circles
     // See https://en.wikipedia.org/wiki/Mercator_projection#Scale_factor for details.
-    let relativeSize = absoluteSize * __cospi(latitude/180.0)
-    return relativeSize
+    absoluteSize * __cospi(latitude/180.0)
   }
   
   var asShape: MKOverlay {
-    if stateCapital || nationalCapital {
+    // TODO: This will depend on the gamemode!
+    if /*stateCapital || */nationalCapital {
       return asStar
     } else {
       return asCircle
@@ -55,30 +58,25 @@ struct City: Codable, Hashable {
   }
   
   private var asCircle: MKCircle {
-    // TODO: This will depend on the gamemode!
     .init(center: coordinates, radius: circleSize)
   }
   
-  var asStar: MKPolygon {
+  private var asStar: MKPolygon {
     let corners = 5
     let smoothness = 0.5
     let angleAdjustment = .pi * 2 / CGFloat(corners * 2)
     let center = MKMapPoint(coordinates)
     
-    // ???
-    let scaleFactor = (200_000 * log10(0.000_019*(population+100_000))+13_000) * 8
+    let scaleFactor = absoluteSize * 8
     
     let coordinates = (0..<10).map { i -> MKMapPoint in
       let scaleFactor = (i.isOdd ? smoothness : 1) * scaleFactor
       let xCoordinate = scaleFactor * cos(.pi/2.0 - i*angleAdjustment) + center.x
       let yCoordinate = scaleFactor * sin(.pi/2.0 - i*angleAdjustment) + center.y
       
-//      return .init(latitude: xCoordinate, longitude: yCoordinate)
       return .init(x: xCoordinate, y: yCoordinate)
     }
     
-    
     return .init(points: coordinates, count: coordinates.count)
-//    return .init(coordinates: coordinates, count: coordinates.count)
   }
 }
