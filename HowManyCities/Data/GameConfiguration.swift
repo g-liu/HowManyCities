745487636b61aecdf36f6bb7c0796de352f27125
Geoffrey Liu <g-liu@noreply.users.github.com>
2022-03-29 21:48:01 -0700

    Decode states

--- HowManyCities/Data/GameConfiguration.swift
+++ HowManyCities/Data/GameConfiguration.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct GameConfiguration: Codable {
+struct GameConfiguration: Decodable {
   // Not all properties included
   let brackets: [Int]
   let hasStates: Bool
@@ -23,10 +23,40 @@ struct GameConfiguration: Codable {
   // {group, label, states: []}
   // states is another nested level... sigh
   // dunt know if we want to handle this case
-//  let states: [State]
+  let states: [State]
 }
 
-struct State: Codable {
-  let value: String
-  let name: String
+struct State: Decodable {
+  var value: String
+  var name: String
+  var states: [State]?
+  
+  enum CodingKeys: String, CodingKey {
+    case value
+    case name
+  }
+  
+  enum AnotherCodingKeys: String, CodingKey {
+    case value = "group"
+    case name = "label"
+    case states
+  }
+  
+  init(from decoder: Decoder) throws {
+    do {
+      let values = try decoder.container(keyedBy: CodingKeys.self)
+      self.value = try values.decode(String.self, forKey: .value)
+      self.name = try values.decode(String.self, forKey: .name)
+      self.states = nil
+    } catch {
+      do {
+        let values = try decoder.container(keyedBy: AnotherCodingKeys.self)
+        self.value = try values.decode(String.self, forKey: .value)
+        self.name = try values.decode(String.self, forKey: .name)
+        self.states = try values.decode([State].self, forKey: .states)
+      } catch {
+        fatalError("You're stupid")
+      }
+    }
+  }
 }
