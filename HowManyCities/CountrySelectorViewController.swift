9dbd5297e7d44bae2be50e99cf3633f03193462f
Geoffrey Liu <g-liu@noreply.users.github.com>
2022-04-23 17:36:33 -0700

    WIP: set selected country

--- HowManyCities/CountrySelectorViewController.swift
+++ HowManyCities/CountrySelectorViewController.swift
@@ -7,17 +7,12 @@
 
 import UIKit
 
-final class CountrySelector: UIPickerView {
-  // Fuck it
-}
-
 protocol CountrySearchDelegate: AnyObject {
   var countries: [State] { get }
 }
 
 
 final class CountrySearchController: UIViewController {
-//  private let countries = ["Afghanistan","Albania","Algeria","Andorra","Angola","Antigua & Deps","Argentina","Armenia","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Central African Rep","Chad","Chile","China","Colombia","Comoros","Congo","Congo {Democratic Rep}","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Greece","Grenada","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland {Republic}","Israel","Italy","Ivory Coast","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Korea North","Korea South","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar, {Burma}","Namibia","Nauru","Nepal","Netherlands","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Romania","Russian Federation","Rwanda","St Kitts & Nevis","St Lucia","Saint Vincent & the Grenadines","Samoa","San Marino","Sao Tome & Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South Africa","South Sudan","Spain","Sri Lanka","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad & Tobago","Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"]
   
   private var specialSectionLabels: [GuessMode] = [.any, .every]
   
@@ -32,17 +27,6 @@ final class CountrySearchController: UIViewController {
     countriesPostSearch.filter { $0.name.starts(with: String(letter)) }
   }
   
-//  private lazy var searchController: UISearchController = {
-//    let searchController = UISearchController(searchResultsController: self)
-//    searchController.delegate = self
-//    searchController.searchResultsUpdater = self
-//    searchController.searchBar.autocapitalizationType = .words
-//    searchController.searchBar.translatesAutoresizingMaskIntoConstraints = false
-//    searchController.searchBar.delegate = self
-//
-//    return searchController
-//  }()
-  
   private lazy var searchBar: UISearchBar = {
     let bar = UISearchBar().autolayoutEnabled
     bar.autocapitalizationType = .words
@@ -55,6 +39,9 @@ final class CountrySearchController: UIViewController {
   private lazy var tableView: UITableView = {
     let tableView = UITableView(frame: .zero, style: .plain)
     tableView.translatesAutoresizingMaskIntoConstraints = false
+    tableView.allowsSelection = true
+    tableView.allowsMultipleSelection = false
+
     return tableView
   }()
   
@@ -113,6 +100,13 @@ extension CountrySearchController: UITableViewDelegate, UITableViewDataSource {
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+    cell.selectionStyle = .none
+    
+    if indexPath == tableView.indexPathForSelectedRow {
+      cell.accessoryType = .checkmark
+    } else {
+      cell.accessoryType = .none
+    }
     
     if indexPath.section == 0 {
       cell.textLabel?.text = specialSectionLabels[indexPath.row].fullDisplayName
@@ -126,6 +120,26 @@ extension CountrySearchController: UITableViewDelegate, UITableViewDataSource {
     cell.textLabel?.text = GuessMode.specific(countryName).fullDisplayName
     
     return cell
+  }
+  
+//  func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+//    tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+//    if let oldIndex = tableView.indexPathForSelectedRow {
+//      tableView.cellForRow(at: oldIndex)?.accessoryType = .none
+//      tableView.reloadRows(at: [indexPath, oldIndex], with: .none)
+//    } else {
+//      tableView.reloadRows(at: [indexPath], with: .none)
+//    }
+//
+//
+//    return indexPath
+//  }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+  }
+  
+  func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+    tableView.cellForRow(at: indexPath)?.accessoryType = .none
   }
 }
 
