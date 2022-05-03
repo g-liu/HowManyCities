6ffd1c4188f2c3e2e8563b261feac029ee7b0148
Geoffrey Liu <g-liu@noreply.users.github.com>
2022-05-03 16:48:51 -0700

    Improve the city page

--- HowManyCities/Stats/CityInfoViewController.swift
+++ HowManyCities/Stats/CityInfoViewController.swift
@@ -6,9 +6,47 @@
 //
 
 import UIKit
+import MapKit
 
 class CityInfoViewController: UIViewController {
-  var tempLabel = UILabel().autolayoutEnabled // TODO: Temporary of course
+   
+  private lazy var mapView: MKMapView = {
+    let map = MKMapView().autolayoutEnabled
+    map.mapType = .mutedStandard
+    map.isPitchEnabled = false
+    map.isScrollEnabled = false
+    map.isMultipleTouchEnabled = false
+    map.isZoomEnabled = false
+    map.isRotateEnabled = false
+    map.pointOfInterestFilter = .excludingAll
+    map.setRegion(.full, animated: true)
+    
+    return map
+  }()
+  
+  private lazy var infoStack: UIStackView = {
+    let stackView = UIStackView().autolayoutEnabled
+    stackView.axis = .vertical
+    stackView.alignment = .leading
+    stackView.spacing = 8.0
+    
+    return stackView
+  }()
+  
+  private lazy var cityLabel: UILabel = {
+    let label = UILabel(text: "", style: UIFont.TextStyle.largeTitle).autolayoutEnabled
+    label.numberOfLines = 1
+    label.font = UIFont.boldSystemFont(ofSize: label.font.pointSize)
+    
+    return label
+  }()
+  
+  private lazy var populationLabel: UILabel = {
+    let label = UILabel(text: "", style: UIFont.TextStyle.body)
+    label.numberOfLines = 1
+    
+    return label
+  }()
   
   var city: City? {
     didSet {
@@ -21,10 +59,23 @@ class CityInfoViewController: UIViewController {
     
     view.backgroundColor = .systemBackground
     
-    view.addSubview(tempLabel)
-    tempLabel.pin(to: view.safeAreaLayoutGuide)
+    infoStack.addArrangedSubview(cityLabel)
+    infoStack.addArrangedSubview(populationLabel)
     
-    tempLabel.numberOfLines = 0
+    view.addSubview(mapView)
+    view.addSubview(infoStack)
+    
+    NSLayoutConstraint.activate([
+      mapView.topAnchor.constraint(equalTo: view.topAnchor),
+      mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      mapView.heightAnchor.constraint(equalToConstant: 200),
+      
+      infoStack.topAnchor.constraint(equalTo: mapView.bottomAnchor, constant: 8.0),
+      infoStack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8.0),
+      infoStack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 8.0),
+      infoStack.bottomAnchor.constraint(lessThanOrEqualTo: view.safeAreaLayoutGuide.bottomAnchor),
+      ])
   }
   
   private func configure(with city: City?) {
@@ -35,15 +86,25 @@ class CityInfoViewController: UIViewController {
     title = "\(city.countryFlag) \(city.nameWithStateAbbr)"
     navigationItem.title = "\(city.countryFlag) \(city.nameWithStateAbbr)"
     
-    tempLabel.text = """
-Name: \(city.name) \(city.capitalDesignation)
-State: \(city.state)
-Territory: \(city.territory)
-Country: \(city.country)
-Population: \(city.population)
-Coordinates: \(city.coordinates)
-Percent of people that guess this city: \(city.percentageOfSessions ?? 0.0)
-"""
+    cityLabel.text = city.name
+    
+    let annotation = MKPointAnnotation()
+    annotation.coordinate = city.coordinates
+    mapView.addAnnotation(annotation)
+    
+    mapView.setRegion(.init(center: annotation.coordinate, span: .full), animated: true)
+    
+    populationLabel.text = "Population: \(city.population.commaSeparated)"
+    
+//    tempLabel.text = """
+//Name: \(city.name) \(city.capitalDesignation)
+//State: \(city.state)
+//Territory: \(city.territory)
+//Country: \(city.country)
+//Population: \(city.population)
+//Coordinates: \(city.coordinates)
+//Percent of people that guess this city: \(city.percentageOfSessions ?? 0.0)
+//"""
   }
   
 }
