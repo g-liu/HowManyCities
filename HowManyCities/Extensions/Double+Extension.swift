bbbe903c61c031b46f63cd51d199adf9302fff21
Geoffrey Liu <g-liu@noreply.users.github.com>
2022-05-06 14:31:19 -0700

    Percentages of very small

--- HowManyCities/Extensions/Double+Extension.swift
+++ HowManyCities/Extensions/Double+Extension.swift
@@ -14,8 +14,12 @@ extension Double {
   var asCGFloat: CGFloat { .init(self) }
   var asNSNumber: NSNumber { .init(value: self) }
   
+  // TODO: Format very small percentages to the first N non-zero decimal places
   var asPercentString: String {
-    let value = Foundation.round(self * 10000.0) / 100.0
+    let value = Foundation.round(self * 100000.0) / 1000.0
+    if self > 0.0 && value == 0 {
+      return "<0.001%"
+    }
     return "\(value)%"
   }
 }
